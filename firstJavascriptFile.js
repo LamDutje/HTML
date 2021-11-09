6e0542b3f6ef553061f305fb11cd5395a90ee8cf
Lam Du <55293293+LamDutje@users.noreply.github.com>
2021-11-09 13:53:21 +0100

    Opdracht Gebruik je eerste Javascript

---
+++ firstJavascriptFile.js
@@ -0,0 +1,26 @@
+console.log("Hello Winc Academy")
+
+let name = 'Lam';
+console.log(name);
+
+let age = '29';
+console.log(typeof age);
+
+let num = 4 + 4;
+console.log(num);
+
+let num1 = 1 + 1;
+console.log(num1);
+
+let num2 = 2 - 2;
+console.log(num2);
+
+let num3 = 3 * 3;
+console.log(num3);
+
+let num4 = 30 / 4;
+console.log(num4);
+
+let num5 = 10 % 30;
+console.log(num5);
+
